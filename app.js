b0298849dd71552df8047c877aad62d382c824b6
masterit15 <masterit15@yandex.ru>
2020-07-27 09:54:30 +0300

    app-ftk new

--- app.js
+++ app.js
@@ -40,7 +40,7 @@ const HostName = config.get('host') || 'localhost'
 async function start() {
     try {
         await db.authenticate();
-        server.listen(PORT, HostName, () => console.log(`App has been started on port ${PORT}...`))
+        server.listen(PORT, '0.0.0.0', () => console.log(`App has been started on port ${PORT}...`))
         console.log('Connection has been established successfully.');
       } catch (error) {
         console.error('Unable to connect to the database:', error);
