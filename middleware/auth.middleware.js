6aea00747b65479ee5d99da543b4ea0a0b4586d4
masterit15 <masterit15@yandex.ru>
2020-08-13 21:53:58 +0300

    log

--- middleware/auth.middleware.js
+++ middleware/auth.middleware.js
@@ -6,6 +6,7 @@ module.exports = (req, res, next) => {
     return next()
   }
   try {
+    console.log(req.headers)
     const token = req.headers.authorization.split(' ')[1] // "Bearer TOKEN"
     
     if (!token) {
